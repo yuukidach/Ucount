bc7f3b09716bb87b49d95037b409a313d3466b8d
yuukidach <chendamailbox@foxmail.com>
2021-04-12 17:46:27 +0800

    fix: fix error in exampleInstrumentedTest

--- app/src/androidTest/java/com/yuukidach/ucount/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/yuukidach/ucount/ExampleInstrumentedTest.java
@@ -7,6 +7,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.InstrumentationRegistry.getTargetContext;
 import static org.junit.Assert.*;
 
 /**
@@ -19,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.yuukidach.ucount", appContext.getPackageName());
     }
